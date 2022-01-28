96c1066fb0f9e3f3a806f0cdc5a6df48c59ad71b
James Keicher <jamesdkeicher@gmail.com>
2022-01-28 12:35:00 -0800

    feat: Add Animal component to render each animal in parade as emoji

--- src/Parade/Parade.js
+++ src/Parade/Parade.js
@@ -1,14 +1,15 @@
 import './Parade.css';
 import React from 'react';
+import Animal from './Animal';
 
 export default function Parade({ parade, handleParade }) {
   console.log(parade);
   return (
     <div className='parade-container'>
       <div className='animals'>
-        {parade.map((animal, i) => {
-          return <span key={animal + i} className='animal-span'>{animal}</span>;
-        })}
+        {parade.map((animal, i) => 
+          <Animal key={animal + i} animal={animal} />
+        )}
       </div>
       <div className='buttons-container'>
         <button className='buttons' onClick={() => handleParade('lion')}>Add Lion</button>
